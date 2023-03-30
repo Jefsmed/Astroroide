0ad4662ed66d2a60a7b37484a237ac4833c1af95
Jefferson Medeiros <jefsmed@outlook.com>
2023-03-30 07:46:15 -0300

    StartProject

---
+++ Source/Astroroide/AstroroideGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AstroroideGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTROROIDE_API AAstroroideGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
